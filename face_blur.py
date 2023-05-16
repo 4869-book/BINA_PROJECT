ff857c8059f4e0a3ec29ee8465e16ec89e47c491
book <bookbigpw@gmail.com>
2023-05-16 20:36:14 +0700

    add blur all

--- face_blur.py
+++ face_blur.py
@@ -39,7 +39,7 @@ class Blurring:
         
         # Define the codec and create VideoWriter object
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        out = cv2.VideoWriter('output_video.mp4', fourcc, 30.0, (int(cap.get(3)),int(cap.get(4))))
+        out = cv2.VideoWriter('output_video.mp4', fourcc, frame_count, (int(cap.get(3)),int(cap.get(4))))
         
         print(f'[INFO] Start bluring process')
         for _ in tqdm(range(frame_count)):
@@ -81,11 +81,57 @@ class Blurring:
         out.release()
         cv2.destroyAllWindows()
 
+    def BlurAll(self):
+        cap = cv2.VideoCapture(self.FootageSource)
+        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        # Define known faces and their names
+        # known_faces = [
+        #     (face2emb(path)) for path in self.FaceTarget
+        # ]
+        
+        # Define the codec and create VideoWriter object
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        out = cv2.VideoWriter('output_video.mp4', fourcc, frame_count, (int(cap.get(3)),int(cap.get(4))))
+        
+        print(f'[INFO] Start bluring process')
+        for _ in tqdm(range(frame_count)):
+            ret, frame = cap.read()
+            if ret == True:
+                # Detect faces in the frame
+                boxes, probs = mtcnn.detect(frame)
+        
+                if boxes is not None:
+                    # Iterate over detected faces
+                    for i, box in enumerate(boxes):
+                        if probs[i] > 0.9:
+                            x1, y1, x2, y2 = box.astype(int)
+                
+                            # Get embedding for detected face
+                            face = frame[y1:y2, x1:x2]
+                        
+                            blur = cv2.blur(face, (25, 25))
+                            frame[y1:y2, x1:x2] = blur    
+
+                # Save the frame to output video
+                out.write(frame)                  
+                
+                cv2.imshow('Frame', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'): # press 'q' to exit
+                    break
+            else:
+                break
+        cap.release()
+        out.release()
+        cv2.destroyAllWindows()
+        
+
+
 if __name__ == "__main__":
     blur = Blurring('../dataset/video7.mp4')
     
-    blur.TargetFace([0])
-
-    blur.BlurProcess()
+    # blur.TargetFace([0])
+    # blur.BlurProcess()
+    blur.BlurAll()
     
     
